4971f9af84be6b958069ad786dcdb3b43ef049fc
kofimeighan <km3547@columbia.edu>
2020-07-01 10:56:51 -0400

    Trying to solve lint checks

--- portfolio/src/main/webapp/js/custom-script.js
+++ portfolio/src/main/webapp/js/custom-script.js
@@ -14,30 +14,57 @@
 /* exported onLoad */
 /* exported codeAddress */
 /* exported marker */
+/* global google */
 // Neccessary constants or else variables will return as
 // 'undefined' in lint checks
-let map;
-let geocoder;
-let marker;
-const google = window.google;
+
 // Center points to the middle of the United States
 const INITIAL_LAT = 39.8283;
 const INITIAL_LNG = -98.5795;
 
 function onLoad() {
-  loadMap();
+  const martyrData = [
+    ['George Floyd', 'Minneapolis, Minnesota'],
+    ['Ahmaud Arbery', 'Brunswick, Georgia'],
+    ['Breonna Taylor', 'Louisville, Kentucky'],
+    ['Rayshard Brooks', 'Atlanta, Georgia'],
+    ['Robert Fuller', 'Palmdale, California'],
+    ['James Scurlock', 'Omaha, Nebraska'],
+    ['Elijah McClain', 'Aurora, Colorado'],
+    ['Placeholder', 'Mountain View, CA'],
+    ['Placeholder', 'Mountain View, CA'],
+  ];
+
+  const ipData = [
+    [
+      'Black panthers storming the California capitol',
+      'California State Capitol, 1315 10th St room b-27, Sacramento, CA 95814'],
+    ['Martin Luther King\'s march on Washington', 'Washington, D.C.'],
+    [
+      'Rev. Al Sharpton\'s march on Washington on August 28th, 2020',
+      'Washington, D.C.'],
+  ];
+  let map = new Map();
+  console.log(map);
+
+  map.loadMap()
+  //loadMap();
+  populateDropdown(martyrData, 'martyr-dropdown-menu');
+  populateDropdown(ipData, 'IP-dropdown-menu');
 }
 
-function loadMap() {
-  geocoder = new google.maps.Geocoder();
+function loadMap() { 
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: INITIAL_LAT, lng: INITIAL_LNG},
     zoom: 4,
     mapTypeId: 'satellite',
   });
+
+  
 }
 
 function codeAddress(address) {
+
   geocoder.geocode({'address': address}, function(results, status) {
     if (status == 'OK') {
       map.setCenter(results[0].geometry.location);
@@ -46,5 +73,23 @@ function codeAddress(address) {
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
     }
+  });
+}
+
+function populateDropdown(list, ID) {
+  const dropDownMenu = document.getElementById(ID);
+  list.forEach((nameAndLocation) => {
+    const listElement = document.createElement('li');
+    listElement.innerText = nameAndLocation[0];
+
+    const titleElement = document.createElement('a');
+    titleElement.innerText = '';
+    listElement.appendChild(titleElement);
+
+    listElement.addEventListener('click', () => {
+      codeAddress(nameAndLocation[1]);
+    });
+
+    dropDownMenu.appendChild(listElement);
   });
 }

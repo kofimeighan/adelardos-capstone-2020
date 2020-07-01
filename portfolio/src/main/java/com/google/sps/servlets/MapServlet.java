8293d9d96621345b41e8c7e7a86dcb2246386026
kofimeighan <km3547@columbia.edu>
2020-07-01 13:49:36 -0400

    Added endpoint

--- portfolio/src/main/java/com/google/sps/servlets/MapServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/MapServlet.java
@@ -17,48 +17,51 @@ package com.google.sps.servlets;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.*;
-import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/maps")
+@WebServlet("/map")
 public class MapServlet extends HttpServlet {
-    private static final String NAME = "name";
-    private static final String PHONE = "phone";
-    private static final String LOCATION = "location";
-    private static final String DESCRIPTION = "description";
-    private static final String TABLE_NAME = "Pins";
-    private static final String TIME_STAMP = "timestamp";
+  private static final String NAME = "name";
+  private static final String PHONE = "phone";
+  private static final String LOCATION = "location";
+  private static final String DESCRIPTION = "description";
+  private static final String TABLE_NAME = "Pins";
+  private static final String TIME_STAMP = "timestamp";
 
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        UserService userService = UserServiceFactory.getUserService();
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        String name = request.getParameter(NAME);
-        String phone = request.getParameter(PHONE);
-        String location = request.getParameter(LOCATION);
-        String description = request.getParameter(DESCRIPTION);
-        long timeStamp = System.currentTimeMillis();
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    System.out.println("IN DO POST----------------------------------");
+    UserService userService = UserServiceFactory.getUserService();
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    String name = request.getParameter(NAME);
+    String phone = request.getParameter(PHONE);
+    String location = request.getParameter(LOCATION);
+    String description = request.getParameter(DESCRIPTION);
+    long timeStamp = System.currentTimeMillis();
 
-        Entity pinEntity = new Entity(TABLE_NAME);
-        pinEntity.setProperty(NAME, name);
-        pinEntity.setProperty("Phone #", phone);
-        pinEntity.setProperty(LOCATION, location);
-        pinEntity.setProperty(DESCRIPTION, description);
-        pinEntity.setProperty(TIME_STAMP, timeStamp);
-        datastore.put(pinEntity);
-    }
+    Entity pinEntity = new Entity(TABLE_NAME);
+    pinEntity.setProperty("Name", name);
+    pinEntity.setProperty("Phone #", phone);
+    pinEntity.setProperty("Address", location);
+    pinEntity.setProperty("Description/Info", description);
+    pinEntity.setProperty("Time Submitted", timeStamp);
+    datastore.put(pinEntity);
+
+    response.setContentType("text/html");
+    response.getWriter().println("TEST");
+    response.sendRedirect("/statistics.html");
+  }
 }

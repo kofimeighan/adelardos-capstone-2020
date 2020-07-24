5ff1440bfe1c7761cb2ced58d7263460057f5771
Bria Fassler <briafassler@gmail.com>
2020-07-24 12:10:10 -0400

    Changed variable name and added TODO

--- portfolio/src/main/java/com/google/sps/servlets/InteractiveChartServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/InteractiveChartServlet.java
@@ -14,22 +14,23 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/interactive-chart")
 public class InteractiveChartServlet extends HttpServlet {
   private Map<String, Integer> emotionVotes = new HashMap<>();
-  private static String emotionData = "emotion";
+  private static String EMOTION_DATA_PARAM = "emotion";
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("application/json");
     Gson gson = new Gson();
     String json = gson.toJson(emotionVotes);
+    response.setContentType("application/json");
     response.getWriter().println(json);
   }
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String emotion = request.getParameter(emotionData);
+    String emotion = request.getParameter(EMOTION_DATA_PARAM);
     int currentVotes = emotionVotes.containsKey(emotion) ? emotionVotes.get(emotion) : 0;
     emotionVotes.put(emotion, currentVotes + 1);
 
+    //TODO(briafassler): Find a way to prevent redirecting when submitting votes
     response.sendRedirect("/statistics.html");
   }
 }
